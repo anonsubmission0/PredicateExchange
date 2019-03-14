6bd03c3ba5828dd6f1634a358c9f3b6eb066a5bd
anonsubmission0 <anonsubmission0@gmail.com>
2019-03-14 04:28:15 -0400

    invraytrace

---
+++ InvRayTrace.jl/src/InvRayTrace.jl
@@ -0,0 +1,38 @@
+__precompile__(false)
+"Inverse Graphics"
+module InvRayTrace
+
+using Omega
+using RayTrace
+using JLD2
+using FileIO
+using Flux
+using DataFrames
+import Statistics: mean
+using Random
+using Callbacks
+
+import RayTrace: ListScene, rgbimg, rgb, msphere, Vec3, Sphere, Scene, render, MaterialGeom
+# import GeometryTypes: Point, Vec3
+using FileIO
+using DataFrames
+
+include("img.jl")
+include("net.jl")
+include("prior.jl")
+
+# export img, scene, img_obs
+
+# include("notebook.jl")
+
+# Optional
+# using OmegaModels
+# using Tensorboard
+# using RunTools
+# using Lens
+# using ZenUtils
+# include("diagnostics.jl")
+# include("hyper.jl")
+# include("samplediag.jl")
+
+end
